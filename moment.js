c29b143e3e2967e073eb35518f9989beb4a3302a
Dodo888 <darkdodo888@gmail.com>
2015-11-10 02:37:53 +0500

    The First One
    Will design the code later

--- moment.js
+++ moment.js
@@ -3,18 +3,63 @@
 module.exports = function () {
     return {
         // Здесь как-то хранится дата ;)
-        date: null,
+        date: {
+            day: 0,
+            hours: 0,
+            minutes: 0
+        },
+
+        dateInMinutes: function () {
+            return this.date.day * 24 * 60 + this.date.hours * 60 + this.date.minutes;
+        },
 
         // А здесь часовой пояс
-        timezone: null,
+        timezone: 5,
+
+        increase: function () {
+            this.date.minutes++;
+            if (this.date.minutes == 60) {
+                this.date.minutes = 0;
+                this.date.hours++;
+            }
+            if (this.date.hours == 24) {
+                this.date.hours = 0;
+                this.date.day++;
+            }
+        },
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            pattern = pattern.replace('%DD', ['ПН', 'ВТ', 'СР'][this.date.day +
+                Math.floor((this.date.hours + this.timezone) / 24)]);
+            pattern = pattern.replace('%HH', (this.date.hours + this.timezone) % 24);
+            if (this.date.minutes < 10) {
+                pattern = pattern.replace('%MM', '0' + this.date.minutes);
+            } else {
+                pattern = pattern.replace('%MM', this.date.minutes);
+            }
+            return pattern;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            var differenceInMinutes = (moment.date.day - this.date.day) * 24 * 60 +
+                (moment.date.hours - this.date.hours) * 60 +
+                (moment.date.minutes - this.date.minutes);
+            var resultString = '';
+            if (differenceInMinutes >= 24 * 60) {
+                resultString += Math.floor(differenceInMinutes / 24 * 60) + ' дней, ';
+            }
+            differenceInMinutes = differenceInMinutes % (24 * 60);
+            if (differenceInMinutes >= 60) {
+                resultString += Math.floor(differenceInMinutes / 60) + ' часов, ';
+            }
+            differenceInMinutes = differenceInMinutes % 60;
+            if (differenceInMinutes == 0) {
+                resultString += differenceInMinutes + ' минут';
+            }
+            return resultString;
         }
     };
 };
--- robbery.js
+++ robbery.js
@@ -5,7 +5,15 @@ var moment = require('./moment');
 // Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var appropriateMoment = moment();
-
+    var gangTimetable = JSON.parse(json);
+    var workingMoments = {
+        start: workingHoursToMoment(workingHours.from),
+        end: workingHoursToMoment(workingHours.to)
+    };
+    while (!isInWorkingHours(workingMoments, appropriateMoment) ||
+        !isGangFree(gangTimetable, appropriateMoment)) {
+        appropriateMoment.increase();
+    }
     // 1. Читаем json
     // 2. Находим подходящий ближайший момент начала ограбления
     // 3. И записываем в appropriateMoment
@@ -13,8 +21,72 @@ module.exports.getAppropriateMoment = function (json, minDuration, workingHours)
     return appropriateMoment;
 };
 
+function workingHoursToMoment(workingHours) {
+    var workingTimes = workingHours.split(/[ :+-]/);
+    var currentMoment = moment();
+    currentMoment.date.hours = parseInt(workingTimes[0]);
+    currentMoment.date.minutes = parseInt(workingTimes[1]);
+    currentMoment.timezone = parseInt(workingTimes[2]);
+    if (workingHours[5] == '-') {
+        currentMoment.timezone *= -1;
+    }
+    currentMoment.date.hours -= currentMoment.timezone;
+    if (currentMoment.date.hours < 0) {
+        currentMoment.date.day--;
+        currentMoment.date.hours += 24;
+    }
+    if (currentMoment.date.hours > 24) {
+        currentMoment.date.day++;
+        currentMoment.date.hours -= 24;
+    }
+    return currentMoment;
+}
+
+function timeToMoment(time) {
+    var workingTimes = time.split(/[ :+-]/);
+    var currentMoment = moment();
+    currentMoment.date.day = ['ПН', 'ВТ', 'СР'].indexOf(workingTimes[0]);
+    currentMoment.date.hours = parseInt(workingTimes[1]);
+    currentMoment.date.minutes = parseInt(workingTimes[2]);
+    currentMoment.timezone = parseInt(workingTimes[3]);
+    if (time[8] == '-') {
+        currentMoment.timezone *= -1;
+    }
+    currentMoment.date.hours -= currentMoment.timezone;
+    if (currentMoment.date.hours < 0) {
+        currentMoment.date.day--;
+        currentMoment.date.hours += 24;
+    }
+    if (currentMoment.date.hours > 24) {
+        currentMoment.date.day++;
+        currentMoment.date.hours -= 24;
+    }
+
+    return currentMoment;
+}
+
+function isInWorkingHours(workingHours, appropriateMoment) {
+    return (workingHours.start.dateInMinutes() <= (appropriateMoment.dateInMinutes() % (24 * 60)) &&
+        (workingHours.end.dateInMinutes() >= (appropriateMoment.dateInMinutes() % (24 * 60))));
+}
+
+function isGangFree(gang, appropriateMoment) {
+    for (var bandit = 0; bandit < Object.keys(gang).length; bandit++) {
+        for (var deal = 0; deal < gang[Object.keys(gang)[bandit]].length; deal++) {
+            if (!((timeToMoment(gang[Object.keys(gang)[bandit]][deal].to).dateInMinutes() <=
+                appropriateMoment.dateInMinutes()) ||
+                (timeToMoment(gang[Object.keys(gang)[bandit]][deal].from).dateInMinutes() >
+                appropriateMoment.dateInMinutes() + 90))) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 // Возвращает статус ограбления (этот метод уже готов!)
 module.exports.getStatus = function (moment, robberyMoment) {
+    var moment = timeToMoment(moment.date);
     if (moment.date < robberyMoment.date) {
         // «До ограбления остался 1 день 6 часов 59 минут»
         return robberyMoment.fromMoment(moment);
